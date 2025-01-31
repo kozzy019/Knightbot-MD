91f07e90ac0421ba22d9661e64923e9a1b814de9
kozzy019 <118377485+kozzy019@users.noreply.github.com>
2025-01-31 09:40:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Whatsapp AI",
   botOwner: 'Kosoko', // Your name
-  ownerNumber: '2348063342267', //Your number
+  ownerNumber: '17373438445', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
